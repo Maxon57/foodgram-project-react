7cb1c78ea13384bf01ba1be724daf883c68d1f72
mignatov <ignatov.mack@gmail.com>
2022-10-25 13:14:48 +0300

    Созданы приложения accounts, recipes, api

---
+++ backend/accounts/models.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from .validators import UsernameValidator
+
+
+class User(AbstractUser):
+    """Кастомная модель User"""
+    username_validator = UsernameValidator()
+
+    username = models.CharField('username',
+                                max_length=settings.NAME_LENGTH,
+                                unique=True,
+                                validators=[username_validator],
+                                error_messages={
+                                    'unique': 'Пользователь с таким именем уже существует.'
+                                }
+                                )
+    email = models.EmailField("email",
+                              max_length=settings.EMAIL_LENGTH,
+                              unique=True
+                              )
+    first_name = models.CharField('Имя',
+                                  max_length=settings.NAME_LENGTH
+                                  )
+    last_name = models.CharField('Фамилия',
+                                 max_length=settings.NAME_LENGTH
+                                 )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        db_table = 'User'
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
