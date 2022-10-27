16b7340d5ea746466a49495ba49c302370247f9f
mignatov <ignatov.mack@gmail.com>
2022-10-27 14:19:24 +0300

    Заполнена моедль Follow

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -9,24 +9,28 @@ class User(AbstractUser):
     """Кастомная модель User"""
     username_validator = UsernameValidator()
 
-    username = models.CharField('username',
-                                max_length=settings.NAME_LENGTH,
-                                unique=True,
-                                validators=[username_validator],
-                                error_messages={
-                                    'unique': 'Пользователь с таким именем уже существует.'
-                                }
-                                )
-    email = models.EmailField("email",
-                              max_length=settings.EMAIL_LENGTH,
-                              unique=True
-                              )
-    first_name = models.CharField('Имя',
-                                  max_length=settings.NAME_LENGTH
-                                  )
-    last_name = models.CharField('Фамилия',
-                                 max_length=settings.NAME_LENGTH
-                                 )
+    username = models.CharField(
+        'username',
+        max_length=settings.NAME_LENGTH,
+        unique=True,
+        validators=[username_validator],
+        error_messages={
+            'unique': 'Пользователь с таким именем уже существует.'
+        }
+    )
+    email = models.EmailField(
+        "email",
+        max_length=settings.EMAIL_LENGTH,
+        unique=True
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=settings.NAME_LENGTH
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=settings.NAME_LENGTH
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
@@ -38,3 +42,23 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Follow(models.Model):
+    """Класс определяет модель для подписок."""
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name='follower'
+                             )
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='following'
+                               )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = (models.UniqueConstraint(
+            fields=('user', 'author'),
+            name='unique_user_author',
+        ),)
